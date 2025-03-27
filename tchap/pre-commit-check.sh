6fe3b0229ece5b298df536677d0f02120d1c4170
mcalinghee <mcalinghee.dev@gmail.com>
2025-03-27 18:00:48 +0100

    add feature flag to enable login by email add login by email

--- tchap/pre-commit-check.sh
+++ tchap/pre-commit-check.sh
@@ -1,6 +1,28 @@
 #!/bin/sh
 
 set -eu
+
+
+# ```sh
+# # Install the CLI
+# cargo install sqlx-cli --no-default-features --features postgres
+
+# cd crates/storage-pg/ # Must be in the mas-storage-pg crate folder
+# export DATABASE_URL=postgresql:///matrix_auth
+# cargo sqlx prepare
+# ```
+
+# ## Migrations
+
+# Migration files live in the `migrations` folder in the `mas-core` crate.
+
+# ```sh
+# cd crates/storage-pg/ # Again, in the mas-storage-pg crate folder
+# export DATABASE_URL=postgresql:///matrix_auth
+# cargo sqlx migrate run # Run pending migrations
+# cargo sqlx migrate add [description] # Add new migration files
+# ```
+
 # cd ..
 # ./misc/update.sh
 # cargo +nightly fmt 
