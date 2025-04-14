06db5a9a40b9625fafee2a6605bdb16cbc0650a4
olivier <odelcroi@gmail.com>
2025-04-14 14:57:26 +0200

    add tchap filters for jinja

--- crates/handlers/src/upstream_oauth2/template.rs
+++ crates/handlers/src/upstream_oauth2/template.rs
@@ -7,6 +7,7 @@
 use std::{collections::HashMap, sync::Arc};
 
 use base64ct::{Base64, Base64Unpadded, Base64Url, Base64UrlUnpadded, Encoding};
+use mas_tchap;
 use minijinja::{
     Environment, Error, ErrorKind, Value,
     value::{Enumerator, Object},
@@ -187,6 +188,12 @@ pub fn environment() -> Environment<'static> {
     env.add_filter("tlvdecode", tlvdecode);
     env.add_filter("string", string);
     env.add_filter("from_json", from_json);
+    
+    // Add Tchap-specific filters, this could be a generic config submitted 
+    // to upstream allowing all users to add their own filters without upstream code modifications
+    // tester les fonctions async pour le reseau
+    env.add_filter("email_to_display_name", |s: &str| mas_tchap::email_to_display_name(s));
+    env.add_filter("email_to_mxid_localpart", |s: &str| mas_tchap::email_to_mxid_localpart(s));
 
     env.set_unknown_method_callback(minijinja_contrib::pycompat::unknown_method_callback);
 
