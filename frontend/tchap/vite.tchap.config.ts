0c12594c6196078cec312a5458508511100bd63d
olivier <odelcroi@gmail.com>
2025-08-22 16:14:14 +0200

    fix lint

--- frontend/tchap/vite.tchap.config.ts
+++ frontend/tchap/vite.tchap.config.ts
@@ -1,6 +1,6 @@
-import { mergeConfig, defineConfig } from "vite";
-import viteConfig from "../vite.config";
 import { resolve } from "node:path";
+import { defineConfig, mergeConfig } from "vite";
+import viteConfig from "../vite.config";
 
 export default defineConfig((env) =>
   mergeConfig(
@@ -12,7 +12,10 @@ export default defineConfig((env) =>
           input: [
             resolve(__dirname, "css/tchap.css"),
             //resolve(__dirname, "../node_modules/@gouvfr-lasuite/integration/dist/css/prefixed-dsfr.css"),
-            resolve(__dirname, "../node_modules/@gouvfr-lasuite/integration/dist/css/homepage-full.css"),
+            resolve(
+              __dirname,
+              "../node_modules/@gouvfr-lasuite/integration/dist/css/homepage-full.css",
+            ),
           ],
         },
       },
