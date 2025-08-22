e48bcbd434b138e2e3e91df7e2b379993c577fd5
olivier <odelcroi@gmail.com>
2025-08-22 15:58:51 +0200

    add lasuite integration resources

--- frontend/tchap/vite.tchap.config.ts
+++ frontend/tchap/vite.tchap.config.ts
@@ -1,19 +1,21 @@
-import { mergeConfig, defineConfig } from 'vite';
-import viteConfig from '../vite.config';
+import { mergeConfig, defineConfig } from "vite";
+import viteConfig from "../vite.config";
 import { resolve } from "node:path";
 
-export default defineConfig((env) => mergeConfig(
-  viteConfig(env),
-  defineConfig({
-    //tchap config
-   build:{
-     rollupOptions: {
-      input: [
-        resolve(__dirname, "src/tchap.css"),
-         ]
-      }
-   }
-}
-    
+export default defineConfig((env) =>
+  mergeConfig(
+    viteConfig(env),
+    defineConfig({
+      //tchap config
+      build: {
+        rollupOptions: {
+          input: [
+            resolve(__dirname, "css/tchap.css"),
+            //resolve(__dirname, "../node_modules/@gouvfr-lasuite/integration/dist/css/prefixed-dsfr.css"),
+            resolve(__dirname, "../node_modules/@gouvfr-lasuite/integration/dist/css/homepage-full.css"),
+          ],
+        },
+      },
+    }),
   ),
-));
+);
