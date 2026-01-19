9f9d31a7e40c3be8485311a93cbde540a8dc5641
olivierdelcroix <odelcroi@gmail.com>
2026-01-19 17:07:43 +0100

    :tchap: add org.matrix.msc3824.action for tchap legacy compatibility

--- crates/handlers/src/compat/login_sso_redirect.rs
+++ crates/handlers/src/compat/login_sso_redirect.rs
@@ -22,6 +22,9 @@ use crate::impl_from_error_for_route;
 pub struct Params {
     #[serde(rename = "redirectUrl")]
     redirect_url: Option<String>,
+    //:tchap: add params renaming for mobile legacy tchap devices
+    #[serde(rename = "org.matrix.msc3824.action")]
+    //:tchap: end
     action: Option<CompatLoginSsoAction>,
 }
 
