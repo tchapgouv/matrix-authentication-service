60e0f569a825cc2079c7d6d413b3b957574217b5
olivierdelcroix <odelcroi@gmail.com>
2026-04-28 09:08:13 +0200

    remove org.matrix.msc3824.action patch

--- crates/handlers/src/compat/login_sso_redirect.rs
+++ crates/handlers/src/compat/login_sso_redirect.rs
@@ -22,9 +22,6 @@ use crate::impl_from_error_for_route;
 pub struct Params {
     #[serde(rename = "redirectUrl")]
     redirect_url: Option<String>,
-    //:tchap: add params renaming for mobile legacy tchap devices
-    #[serde(rename = "org.matrix.msc3824.action")]
-    //:tchap: end
     action: Option<CompatLoginSsoAction>,
 
     #[serde(rename = "org.matrix.msc3824.action")]
